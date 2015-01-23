6a44f4542ff935f350fae64cceb1595ae5407eec
Mike Spivey <mike@cs.ox.ac.uk>
2015-01-23 15:08:31 +0000

    Added Keiko build config for Darwin64

--- keiko/config.mk
+++ keiko/config.mk
@@ -4,18 +4,19 @@ HOST := $(shell uname -s)-$(shell uname -m)
 ifeq ($(HOST),Linux-i686)
   OK = 1
   CC = gcc
-  TEST_PPC2 = 1
-  PAS0 = pas0.o
 endif
 
 ifeq ($(HOST),Linux-x86_64)
   OK = 1
   CC = gcc -m32
-  TEST_PPC2 = 1
-  PAS0 = pas0.o
 endif
 
 ifeq ($(HOST),Darwin-i386)
+  OK = 1
+  CC = gcc -m32
+endif
+
+ifeq ($(HOST),Darwin-x86_64)
   OK = 1
   CC = gcc -m32
 endif
